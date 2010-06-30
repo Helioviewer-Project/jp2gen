77600802a5c0c60e6550afaa9824f2982c40e338
Jack.Ireland@nasa.gov <>
2010-06-30 17:54:34 -0400

    beginning of a python script to pull JP2 files from LMSAL

---
+++ aia/hv_aia_pull.py
@@ -0,0 +1,27 @@
+from os.path import basename
+from urlparse import urlsplit
+import shutil
+import urllib2
+
+def download(url, fileName=None):
+    def getFileName(url,openUrl):
+        if 'Content-Disposition' in openUrl.info():
+            # If the response has Content-Disposition, try to get filename from it
+            cd = dict(map(
+                lambda x: x.strip().split('=') if '=' in x else (x.strip(),''),
+                openUrl.info().split(';')))
+            if 'filename' in cd:
+                filename = cd['filename'].strip("\"'")
+                if filename: return filename
+        # if no filename was found above, parse it out of the final URL.
+        return basename(urlsplit(openUrl.url)[2])
+
+    r = urllib2.urlopen(urllib2.Request(url))
+    try:
+        fileName = fileName or getFileName(url,r)
+        with open(fileName, 'wb') as f:
+            shutil.copyfileobj(r,f)
+    finally:
+        r.close()
+
+download('http://sdowww.lmsal.com/sdomedia/hv_jp2kwrite/v0.8/jp2/AIA/94/2010/06/18/2010_06_18__00_00_20_135__SDO_AIA_AIA_94.jp2')
