a64c3b0af7454db4b1809b0a0f2864ca0149488b
Jack.Ireland@nasa.gov <>
2011-02-09 15:35:58 -0500

    changes to file pulling script. now uses Python time stamps.

--- scripts/hv_file_pull.py
+++ scripts/hv_file_pull.py
@@ -14,27 +14,22 @@
 #
 #
 """
-Creates an SQLite database with entries in the following order
-nickname text,
-yyyy text,
-mm text,
-dd text,
-hh text,
-mmm text,
-ss text,
-milli text,
-observationTime real,
-measurement text,
-downloadedTimeStamp text,
-downloadedWhenTimeStart real,
-downloadedWhenTimeEnd real,
-downloadedFrom text,
-downloadedFilename text,
-goodfile 
+Reads a setup file, downloads files, and populates an SQLite
+database with the entries.
+The SQLite database is populated with the following entries
+filename text = the filename of the file downloaded
+nickname text = nickname of the instrument from which the file came
+measurement text = measurement of the instrument
+observationTimeStamp text (Python datetime timestamp) = observation time stamp in UTC
+downloadTimeStart text (Python datetime timestamp) = when the download started in UTC
+downloadTimeEnd text (Python datetime timestamp) = when the download ended in UTC
+logFileName text = name of the wget log file
+isFileGood int = is the file good (1) or bad (0)
+fileProblem int = identified problems with the quarantined file; 0 if there are no problems
 """
 from urlparse import urlsplit
 from sgmllib import SGMLParser
-import shutil, urllib2, urllib, os, time, sys, calendar, sqlite3
+import shutil, urllib2, urllib, os, time, sys, calendar, sqlite3, datetime
 
 # URLLister
 class URLLister(SGMLParser):
@@ -84,7 +79,7 @@ def isFileGood(fullPathAndFilename,minimumFileSize,endsWith=''):
 	""" Tests to see if a file meets the minimum requirements to be ingested into the database.
 	An entry of -1 means that the test was not performed, 0 means failure, 1 means pass.
 	"""
-	answer = {"fileExists":-1,"minimumFileSize":-1,"isFileGoodDB":-1}
+	answer = {"fileExists":-1,"minimumFileSize":-1,"isFileGoodDB":-1,"fileProblem":-1}
 
 	# Does the file exist?
 	if os.path.isfile(fullPathAndFilename):
@@ -94,6 +89,7 @@ def isFileGood(fullPathAndFilename,minimumFileSize,endsWith=''):
 		if s.st_size > minimumFileSize:
 			answer["minimumFileSize"] = 1
 		else:
+			answer["fileProblem"] = answer["fileProblem"] + 2
 			answer["minimumFileSize"] = 0
 		
 		# test that the file has the right extension
@@ -101,8 +97,10 @@ def isFileGood(fullPathAndFilename,minimumFileSize,endsWith=''):
 			if fullPathAndFilename.endswith(endsWith):
 				answer["endsWith"] = 1
 			else:
+				answer["fileProblem"] = answer["fileProblem"] + 4
 				answer["endsWith"] = 0
 	else:
+		answer["fileProblem"] = answer["fileProblem"] + 1
 		answer["fileExists"] = 0
 
 	# Has the file passed all the tests?
@@ -115,7 +113,7 @@ def isFileGood(fullPathAndFilename,minimumFileSize,endsWith=''):
 	answer["isFileGood"] = isFileGood
 	answer["isFileGoodDB"] = isFileGoodDB
 
-	return {"isFileGood":isFileGood,"fileExists":answer["fileExists"],"minimumFileSize":answer["minimumFileSize"],"isFileGoodDB":answer["isFileGoodDB"]}
+	return {"isFileGood":isFileGood,"fileExists":answer["fileExists"],"minimumFileSize":answer["minimumFileSize"],"isFileGoodDB":answer["isFileGoodDB"],"fileProblem":answer["fileProblem"]}
 
 
 # createTimeStamp
@@ -176,6 +174,17 @@ def hvParseJP2Filename(filename):
 	# return (int(D[0]),int(D[1]),int(D[2]),int(T[0]),int(T[1]),int(T[2]),int(T[3]),obs[0],obs[1],obs[2],obs[3])
 	return {'date':[int(D[0]),int(D[1]),int(D[2])], 'time':[int(T[0]),int(T[1]),int(T[2]),int(T[3])], 'observation':obs}
 
+# hvParseJP2FilenameToString
+def hvParseJP2FilenameToString(filename):
+	""" Parse a Helioviewer JP2 filename into its component parts """
+	z0 = filename.split('.')
+	z1 = z0[0].split('__')
+	D = z1[0].split('_')
+	T = z1[1].split('_')
+	obs = z1[2].split('_')
+	# return (int(D[0]),int(D[1]),int(D[2]),int(T[0]),int(T[1]),int(T[2]),int(T[3]),obs[0],obs[1],obs[2],obs[3])
+	return {'date':[D[0],D[1],D[2]], 'time':[T[0],T[1],T[2],T[3]], 'observation':obs}
+
 # hvJP2FilenameToTimeInSeconds
 def hvJP2FilenameToTimeInSeconds(filename):
 	""" Convert a JP2 file name into a time in seconds after epoch."""
@@ -183,12 +192,17 @@ def hvJP2FilenameToTimeInSeconds(filename):
 	t = (p['date'][0],p['date'][1],p['date'][2],p['time'][0],p['time'][1],p['time'][2])
 	return calendar.timegm(t)
 
-# hvJP2FilenameToTimeInMilliseconds
-def hvJP2FilenameToTimeInMilliseconds(filename):
-	""" Convert a JP2 file name into a time in milliseconds after epoch."""
-	seconds = hvJP2FilenameToTimeInSeconds(filename)
-	p = hvParseJP2Filename(filename)
-	return 1000.0*seconds + p['time'][3]
+# hvJP2FilenameToTimeStamp
+def hvJP2FilenameToTimeStamp(filename):
+	""" Convert a JP2 file name to a timestamp."""
+	p = hvParseJP2FilenameToString(filename)
+	milli = p['time'][3]
+	if len(milli) == 1:
+		milli = milli + '50'
+	else:
+		if len(milli) == 2:
+			milli = milli + '5'
+	return p['date'][0]+'-'+p['date'][1]+'-'+p['date'][2]+' '+p['time'][0]+':'+p['time'][1]+':'+p['time'][2]+'.'+ milli
 
 # hvCheckForNewFiles
 def hvCheckForNewFiles(urls,List):
@@ -253,12 +267,12 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 	try:
 		if not os.path.isfile(dbloc + dbName):
 			jprint('Creating database and connecting to it = ' + dbloc + dbName)
-			conn = sqlite3.connect(dbloc + dbName)
+			conn = sqlite3.connect(dbloc + dbName, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 			c = conn.cursor()
-			c.execute('''create table TableTest (filename text, nickname text, measurement text, yyyy int, mm int, dd int, observationTimeInMilliseconds real, logFileName text, downloadTimeStart real, downloadTimeEnd real, isFileGood int)''')
+			c.execute('''create table TableTest (filename text, nickname text, measurement text, observationTimeStamp timestamp, downloadTimeStart timestamp, downloadTimeEnd timestamp, logFileName text, isFileGood int, fileProblem int)''')
 		else:
 			jprint('Connecting to database = ' + dbloc + dbName)
-			conn = sqlite3.connect(dbloc + dbName)
+			conn = sqlite3.connect(dbloc + dbName, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 			c = conn.cursor()
 	except Exception,error:
 		jprint('Exception caught creating a new database file; error: '+str(error))
@@ -290,20 +304,30 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
         # Calculate the remote directory
         remote_location = remote_root + nickname + '/' + hvss[-1]
 
+	# Query start and end times
+	queryTimeStart = yyyy + '-' + mm + '-' + dd + ' 00:00:00.000'
+	queryTimeEnd   = yyyy + '-' + mm + '-' + dd + ' 23:59:59.999'
+
+	print queryTimeStart
+	print queryTimeEnd
+
 	# Now query the database: first compare the DB to the contents of the local staging directory, and then to the contents of the remote directory
 	for i in range(0,2):
 		# Database: Find the good files for this nickname, date and measurement.  Return the JP2 filenames
 		try:
-			query = (nickname,yyyy,mm,dd,measurement,1)
-			c.execute('SELECT filename FROM TableTest WHERE nickname=? AND yyyy=? AND mm=? AND dd=? AND measurement=? AND isFileGood =?',query)
+			query = (nickname,measurement,queryTimeStart,queryTimeEnd,1)
+			c.execute('SELECT filename FROM TableTest WHERE nickname=? AND measurement=? and observationTimeStamp>? and observationTimeStamp<? AND isFileGood=?',query)
+			#query = (nickname,measurement,1)
+			#c.execute('SELECT filename FROM TableTest WHERE nickname=? AND measurement=? AND isFileGood=?',query)
 			jp2list_good = c.fetchall()
 		except Exception,error:
 			jprint('Exception found querying database for the bad files from the database; error: '+str(error))
+		print jp2list_good
 
 		# Database: Find the bad files for this nickname, date and measurement.  Return the JP2 filenames
 		try:
-			query = (nickname,yyyy,mm,dd,measurement,0)
-			c.execute('SELECT filename FROM TableTest WHERE nickname=? AND yyyy=? AND mm=? AND dd=? AND measurement=? AND isFileGood =?',query)
+			query = (nickname,measurement,queryTimeStart,queryTimeEnd,0)
+			c.execute('SELECT filename FROM TableTest WHERE nickname=? AND measurement=? and observationTimeStamp>? and observationTimeStamp<? AND isFileGood=?',query)
 			jp2list_bad = c.fetchall()
 		except Exception,error:
 			jprint('Exception found querying database for the bad files from the database; error: '+str(error))
@@ -328,7 +352,7 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 
 				# Download the new files
 				# When the download started
-				downloadTimeStart = calendar.timegm(time.gmtime())
+				downloadTimeStart = datetime.datetime.utcnow()
 				jprint('Acquiring new files.')
 				if beginsWithHTTP(fileLocation):
 					localLog = ' -a ' + logSubdir + logFileName + ' '
@@ -342,7 +366,7 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 						jprint('Exception caught at executing wget command; error: '+str(error))
 
 				# When the download time ends
-				downloadTimeEnd = calendar.timegm(time.gmtime())
+				downloadTimeEnd = datetime.datetime.utcnow()
 
 				# Get the filenames
 				f = open(newFileListFullPath,'r')
@@ -360,24 +384,25 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 					# return the analysis on each file
 					analyzeFile = isFileGood(staged,  minJP2SizeInBytes, endsWith = '.jp2')
 					
-					# Observation time in milliseconds of the file
-					milli = hvJP2FilenameToTimeInMilliseconds(downloaded)
+					# Observation time stamp
+					observationTimeStamp = hvJP2FilenameToTimeStamp(downloaded)
+					print observationTimeStamp
 
 					# Is the staged file good?
 					if not analyzeFile['isFileGood']:
 						# Quarantine the staged file and update the database
 						info = hvDoQuarantine(quarantine,hvss[-1],downloaded,staged)
-						if (filename,) in jp2list_bad:
+						if (downloaded,) in jp2list_bad:
 							# File is already in the DB as bad: update the details
 							jprint('Quarantined file: updating database entry for file = ' + downloaded)
-							ttt = (downloadTimeStart,downloadTimeEnd,logFileName,filename)
+							ttt = (downloadTimeStart,downloadTimeEnd,logFileName,downloaded)
 							c.execute('UPDATE TableTest SET downloadTimeStart=?,downloadTimeEnd=?,logFileName=? WHERE filename=?',ttt)
 							conn.commit()
 						else:
 							# New bad file: enter it into the DB
 							jprint('Quarantined file: creating database entry for file = ' + downloaded)
-							ttt = (downloaded,nickname,measurement,yyyy,mm,dd,milli,newFileListName,downloadTimeStart,downloadTimeEnd,0)
-							c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?,?,?)',ttt)
+							ttt = (downloaded,nickname,measurement,observationTimeStamp,downloadTimeStart,downloadTimeEnd,newFileListName,0,analyzeFile['fileProblem'])
+							c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?)',ttt)
 							conn.commit()
 					else:
 						# file is good - move it to the ingestion directory
@@ -388,17 +413,17 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 						if not analyzeFile['isFileGood']:
 							# Quarantine the ingested file and update the database
 							info = hvDoQuarantine(quarantine,hvss[-1],downloaded,ingested)
-							if (filename,) in jp2list_bad:
+							if (downloaded,) in jp2list_bad:
 							# File is already in the DB as bad: update the details
 								jprint('Quarantined file: updating database entry for file = ' + downloaded)
-								ttt = (downloadTimeStart,downloadTimeEnd,logFileName,filename)
+								ttt = (downloadTimeStart,downloadTimeEnd,logFileName,downloaded)
 								c.execute('UPDATE TableTest SET downloadTimeStart=?,downloadTimeEnd=?,logFileName=? WHERE filename=?',ttt)
 								conn.commit()
 							else:
 							# New bad file: enter it into the DB
 								jprint('Quarantined file: creating database entry for file = ' + downloaded)
-								ttt = (downloaded,nickname,measurement,yyyy,mm,dd,milli,newFileListName,downloadTimeStart,downloadTimeEnd,0)
-								c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?,?,?)',ttt)
+								ttt = (downloaded,nickname,measurement,observationTimeStamp,downloadTimeStart,downloadTimeEnd,newFileListName,0,analyzeFile['fileProblem'])
+								c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?)',ttt)
 								conn.commit()
 						else:
 							jprint('Moved file '+ staged + ' to ' + ingested)
@@ -411,13 +436,13 @@ def GetMeasurement(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,inge
 								# Fix the download end time to now: process has finished.
 								if (downloaded,) in jp2list_bad:
 									jprint('Ingested: updating the database entry for the file = '+ downloaded)
-									ttt = (downloadTimeStart,downloadTimeEnd,logFileName,filename)
+									ttt = (downloadTimeStart,downloadTimeEnd,logFileName,downloaded)
 									c.execute('UPDATE TableTest SET isFileGood=1,downloadTimeStart=?,downloadTimeEnd=?,logFileName=? WHERE filename=?',ttt)
 									conn.commit()
 								else:
 									jprint('Ingested: creating a database entry for file = '+ downloaded)
-									ttt =(downloaded,nickname,measurement,yyyy,mm,dd,milli,newFileListName,downloadTimeStart,downloadTimeEnd, analyzeFile['isFileGoodDB'])
-									c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?,?,?)',ttt)
+									ttt = (downloaded,nickname,measurement,observationTimeStamp,downloadTimeStart,downloadTimeEnd,newFileListName,0,analyzeFile['fileProblem'])
+									c.execute('INSERT INTO TableTest VALUES (?,?,?,?,?,?,?,?,?)',ttt)
 									conn.commit()
 							except Exception,error:
 						       		jprint('Exception caught updating the new database; error: ' + str(error))
@@ -448,6 +473,7 @@ def GetJP2(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,ingest_root,
 
 	# Write a current file to web-space so you know what the script is trying to do right now.
 	currentFile = open(monitorLoc + 'current.log','w')
+	currentFile.write('Acquisition script started at ' + beginTimeStamp +'.\n')
 	currentFile.write('Measurement = ' + measurement +'.\n')
 	currentFile.write('Beginning remote location query number ' + str(count)+ '.\n')
 	currentFile.write("Looking for files on this date = " + yyyy + mm + dd+ '.\n')
@@ -490,7 +516,6 @@ def GetJP2(nickname,yyyy,mm,dd,measurement,remote_root,staging_root,ingest_root,
 		shutil.copy(logSubdir + stderrFileName, monitorLoc + stderrLatestFileName)
 
 	return nfc
-
 
 
 #
