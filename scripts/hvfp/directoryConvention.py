75042b8d5317408a215bf3cca9da002009523246
Jack Ireland <jackireland@gmail.com>
2014-10-09 11:26:56 -0400

    code in the scripts directory has all been superseded long ago by the downloader written by Keith

--- scripts/hvfp/directoryConvention.py
+++
@@ -1,4 +0,0 @@
-# hvSubdir
-def directoryConvention(measurement,yyyy,mm,dd):
-	"""Return the directory structure for helioviewer JPEG2000 files."""
-	return [yyyy+os.sep, yyyy+os.sep+mm+os.sep, yyyy+os.sep+mm+os.sep+dd+os.sep, yyyy+os.sep+mm+os.sep+dd+os.sep + measurement + os.sep]
